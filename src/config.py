fe1d9b41e8e225b2d908c74508a1c7f3e3264f9a
Salvatore <borgesisalvo@gmail.com>
2022-11-22 11:34:44 +0100

    create new CSV (.set)

--- src/config.py
+++ src/config.py
@@ -7,24 +7,24 @@
 LABELS_PATHS = ['./labels.train.txt','./labels.test.txt']
 
 ## Training,Validation and test file tell where to save CSV
-TRAINING_CSV_FILE = "csv/train.new_4.csv"
-VALIDATION_CSV_FILE = "csv/validation.new_4.csv"
-TEST_CSV_FILE="csv/test.new_4.csv"
+TRAINING_CSV_FILE = "csv/train.set.csv"
+VALIDATION_CSV_FILE = "csv/validation.set.csv"
+TEST_CSV_FILE="csv/test.set.csv"
 
 # -- Configuration parameters for DataLoader creation
 
 ## Config information which say from which CSV create train,valid,test set
-TRAINING_CSV_DATASET_FILE ="csv/train.new_3.csv"
-VALIDATION_CSV_DATASET_FILE = "csv/validation.new_3.csv"
-TEST_CSV_DATASET_FILE = "csv/test.new_3.csv"
+TRAINING_CSV_DATASET_FILE ="csv/train.set.csv"
+VALIDATION_CSV_DATASET_FILE = "csv/validation.set.csv"
+TEST_CSV_DATASET_FILE = "csv/test.set.csv"
 
 BATCH_SIZE = 32
 NUM_WORKERS = 3
 
 ## -- Main configuration
 
-MODEL_NAME = "squeezenet"
-EPOCHS = 25
+MODEL_NAME = "alexnet"
+EPOCHS = 30
 LOAD_CHECKPOINT = True
 LEARNING_RATE = 0.0005
 FEATURE_EXTR = False
