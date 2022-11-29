ef9ee59ea3a5750bfddf4ccb363d5a6bf1f15bab
Salvatore <borgesisalvo@gmail.com>
2022-11-29 13:12:20 +0100

    add logs and recpas

--- src/config.py
+++ src/config.py
@@ -36,26 +36,26 @@ TEST_CSV_FILE="csv/test.all.csv"
 # -- Configuration parameters for DataLoader creation
 
 ## Config information which say from which CSV create train,valid,test set
-TRAINING_CSV_DATASET_FILE ="csv/train.all.csv"
-VALIDATION_CSV_DATASET_FILE = "csv/validation.all.csv"
-TEST_CSV_DATASET_FILE = "csv/test.all.csv"
+TRAINING_CSV_DATASET_FILE ="csv/train.set.csv"
+VALIDATION_CSV_DATASET_FILE = "csv/validation.set.csv"
+TEST_CSV_DATASET_FILE = "csv/test.set.csv"
 
 BATCH_SIZE = 32
 NUM_WORKERS = 3
 
 ## -- Main configuration
 
-MODEL_NAME = "squeezenet"
+MODEL_NAME = "alexnet"
 EPOCHS = 50
 LOAD_CHECKPOINT = True
 LEARNING_RATE = 0.001
-FEATURE_EXTR = False
+FEATURE_EXTR = True
 DATA_AUGMENTATION = True
 
 ## -- Train configuration
 
-CHECKPOINT_DIR = "checkpoint/all/epochs="+str(EPOCHS)
-LOG_DIR = "logs/all/epochs="+str(EPOCHS)
+CHECKPOINT_DIR = "checkpoint/epochs="+str(EPOCHS)
+LOG_DIR = "logs/epochs="+str(EPOCHS)
 
 ## -- Gradio configuration
-GRADIO_LOAD_CHECKPOINT_PATH="checkpoint/all/epochs="+str(EPOCHS)
+GRADIO_LOAD_CHECKPOINT_PATH="checkpoint/epochs="+str(EPOCHS)
